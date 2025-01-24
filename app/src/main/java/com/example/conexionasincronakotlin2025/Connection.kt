88864000344ab3f88045d84c88d88853e855584c
Paco <paco.portada@gmail.com>
2025-01-24 09:33:56 +0100

    obtener redirección

--- app/src/main/java/com/example/conexionasincronakotlin2025/Connection.kt
+++ app/src/main/java/com/example/conexionasincronakotlin2025/Connection.kt
@@ -1,5 +1,6 @@
 package com.example.conexionasincronakotlin2025
 
+import android.util.Log
 import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.HttpURLConnection.HTTP_MOVED_PERM
@@ -12,21 +13,32 @@ object Connection {
         val result = Result()
         result.code = 500
 
-        val connection: HttpURLConnection = url!!.openConnection() as HttpURLConnection
+        var connection: HttpURLConnection = url!!.openConnection() as HttpURLConnection
         result.code = connection.responseCode
+
         if (result.code == HttpURLConnection.HTTP_OK)
             result.content = connection.inputStream.bufferedReader().readText()
         else
             if (result.code == HTTP_MOVED_PERM) {
-                result.content = connection.inputStream.bufferedReader().readText()
-                result.message =
-                    "Error: ${result.code.toString()} \n Error en la conexión a $url \n ${result.content}"
-                // hacer una nueva petición
-
+                // hacer una nueva petición a la redirección recibida
+                // obtener cabeceras de un servidor web:
+                // https://javierin.com/visualizar-cabeceras-http-de-un-servidor-remoto/
+                // curl -I http://dam.org.es/ficheros/frases.html
+                val newUrl = connection.getHeaderField("Location")
+                Log.e("redirección", newUrl.toString())
+                connection = URL(newUrl).openConnection() as HttpURLConnection
+                result.code = connection.responseCode
+                if (result.code == HttpURLConnection.HTTP_OK)
+                    result.content = connection.inputStream.bufferedReader().readText()
+                else
+                    result.message =
+                        "Error: ${result.code.toString()} \n Error en la conexión a $url \n Mensaje: ${connection.responseMessage}"
             } else
                 result.message =
                     "Error: ${result.code.toString()} \n Error en la conexión a $url \n Mensaje: ${connection.responseMessage}"
+
         connection.disconnect()
+
         return result
     }
 }
